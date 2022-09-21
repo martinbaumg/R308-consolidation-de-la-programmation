a8906e008ac11418d0be5cd9d4598e0b8a11d0e8
martin baumgaertner <92078725+martinbaumg@users.noreply.github.com>
2022-09-21 13:50:28 +0200

    d

---
+++ TDs/voiture.py
@@ -0,0 +1,11 @@
+class Point:
+    "Definition d'un point geometrique"
+
+a = Point()
+a.x = 1
+a.y = 2
+b = Point()
+b.x = 3
+b.y = 4
+
+print("a : x =", a.x, "y =", a.y, "b : x =", b.x, "y =", b.y)
